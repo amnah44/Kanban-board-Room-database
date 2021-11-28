56e96e9af9318bcb67598dc9b2f2af042fc89a45
Amnah Ali <amnah1li@gmail.com>
2021-11-28 15:33:49 +0300

    clean code in binding adapter

--- app/src/main/java/com/amnah/rdb/utils/BindingAdapter.kt
+++ app/src/main/java/com/amnah/rdb/utils/BindingAdapter.kt
@@ -1,19 +1,11 @@
 package com.amnah.rdb.utils
 
-import android.view.View
-import android.widget.AdapterView
-import android.widget.Toast
-import androidx.appcompat.widget.AppCompatSpinner
 import androidx.databinding.BindingAdapter
-import androidx.databinding.ObservableField
-import androidx.navigation.Navigation
-import androidx.navigation.R
 import androidx.recyclerview.widget.RecyclerView
 import com.amnah.rdb.ui.base.BaseAdapter
 
 @BindingAdapter(value = ["app:items"])
 fun <T> setRecyclerItems(view: RecyclerView, items:List<T>?){
-
     if (items != null){
         ( view.adapter as BaseAdapter<T>?)?.setItem(items)
     }else{
